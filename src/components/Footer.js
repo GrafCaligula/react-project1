e1fe8db8588e58bc2e0f63e31293dcdfd5e4c250
Markus <markus.kloepper@gmx.net>
2022-11-02 17:36:26 +0100

    ende

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,16 @@
+import twitter from "./Twitter Icon.png";
+import facebook from "./Facebook Icon.png";
+import insta from "./Instagram Icon.png";
+import github from "./GitHub Icon.png";
+
 export default function Footer() {
   return (
     <div className="footer-section">
       <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li><img src={twitter} alt="twitter"/></li>
+        <li><img src={facebook} alt="FB"/></li>
+        <li><img src={insta} alt="Insta"/></li>
+        <li><img src={github} alt="GitHub"/></li>
       </ul>
     </div>
   )
--- src/components/Info.js
+++ src/components/Info.js
@@ -1,15 +1,23 @@
 import logo from "./portrait.png";
+import mail from "./Mail.png";
+import linkedin from "./linkedin.png";
 
 export default function Info() {
   return (
     <div className="info-section">
-      <img className="info-img" src={logo} alt="Logo" />;
+      <img className="info-img" src={logo} alt="Logo" />
       <h1 className="info-name">Marko Klöder</h1>
       <p className="info-job">Umschüler Anwendungsentwicklung</p>
-      <p className="info-link"><small>websiteLink</small></p>
+      <p className="info-link">
+        <small>websiteLink</small>
+      </p>
       <div className="info-btn-wrapper">
-        <button className="info-btn1">Email</button>
-        <button className="info-btn2">Linkedin</button>
+        <button className="info-btn1">
+          <img src={mail} alt="mail" />
+          Email
+        </button>
+        <button className="info-btn2">
+        <img src={linkedin} alt="linkedin"/>Linkedin</button>
       </div>
     </div>
   );
